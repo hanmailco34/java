9ae1fe7c093e5464c4d4b1054829234b3562aba1
asd9211 <drrg@naver.com>
2021-03-22 23:57:52 +0900

    ㅅㄷㄴㅅ

--- rdl/src/main/java/syp/rdl/RdlApplication.java
+++ rdl/src/main/java/syp/rdl/RdlApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RdlApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//테스트한다고
 		SpringApplication.run(RdlApplication.class, args);
 	}
 
