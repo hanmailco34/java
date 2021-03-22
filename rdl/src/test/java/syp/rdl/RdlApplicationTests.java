d1f24b1d0ca1c04f30de033616ea03e962a5d8fa
asd9211 <drrg@naver.com>
2021-03-22 21:19:56 +0900

    commit test

---
+++ rdl/src/test/java/syp/rdl/RdlApplicationTests.java
@@ -0,0 +1,13 @@
+package syp.rdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
