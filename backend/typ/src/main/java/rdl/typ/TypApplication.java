575da9c8eae1fafecd58980d07494d4ccea8879a
yh <yh@LAPTOP-L91NDIK9>
2021-03-23 18:02:20 +0900

    backend commit test

---
+++ backend/typ/src/main/java/rdl/typ/TypApplication.java
@@ -0,0 +1,13 @@
+package rdl.typ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypApplication.class, args);
+	}
+
+}
---
+++ backend/typ/src/test/java/rdl/typ/TypApplicationTests.java
@@ -0,0 +1,13 @@
+package rdl.typ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
